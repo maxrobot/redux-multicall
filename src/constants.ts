058ae86d4a57cea113e966221758f5792bd14dca
Max Grabner <max@grabner.co.uk>
2022-01-07 17:00:18 +0000

    updated the gas to see if that enables a private network to be used

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 import type { CallResult, CallState, ListenerOptions } from './types'
 
-export const DEFAULT_CALL_GAS_REQUIRED = 1_000_000
-export const DEFAULT_CHUNK_GAS_REQUIRED = 200_000
-export const CHUNK_GAS_LIMIT = 100_000_000
-export const CONSERVATIVE_BLOCK_GAS_LIMIT = 10_000_000 // conservative, hard-coded estimate of the current block gas limit
+export const DEFAULT_CALL_GAS_REQUIRED = 10_000_000_000
+export const DEFAULT_CHUNK_GAS_REQUIRED = 10_000_000_000
+export const CHUNK_GAS_LIMIT = 10_000_000_000
+export const CONSERVATIVE_BLOCK_GAS_LIMIT = 10_000_000_000 // conservative, hard-coded estimate of the current block gas limit
 
 // Consts for hooks
 export const INVALID_RESULT: CallResult = { valid: false, blockNumber: undefined, data: undefined }
